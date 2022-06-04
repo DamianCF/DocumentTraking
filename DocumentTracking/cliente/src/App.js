44d094b2bedcf2b5e59399dd9eadd14fd749e44b
Ronald Blanco <ronald.blanco.navarro@est.una.ac.cr>
2022-06-03 22:19:35 -0600

    Login fail

--- DocumentTracking/cliente/src/App.js
+++ DocumentTracking/cliente/src/App.js
@@ -1,45 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
-import ListaUsuarios from './ListaUsuarios';
-import AgregarUsuario from './AgregarUsuario';
-import EditarUsuario from './EditarUsuario';
+import './css/App.css';
+import ListaUsuarios from './components/Login';
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
-import AgregarDepartamento from './AgregarDepartamento';
 
 function App() {
   return (
     <div className="App">
-          <nav className ="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container">
-            <a className="navbar-brand" href="/"> Crud Mern Stack</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Inicio</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="agregarusuario">Agregar Usuario</a>
-                </li>
-                
-                <li className="nav-item">
-                  <a className="nav-link" href="agregardepartamento">Agregar Departamento</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
 
     <BrowserRouter>
       <Routes>
           <Route path= '/' element = {<ListaUsuarios/>} exact ></Route>
-          <Route path= '/agregarusuario' element = {<AgregarUsuario/>} exact></Route>
-          <Route path= '/editarusuario/:idusuario' element = {<EditarUsuario/>} exact></Route>
-          <Route path= '/agregardepartamento' element = {<AgregarDepartamento/>} exact></Route>
       </Routes>
-
     </BrowserRouter>
 
     </div>
---
+++ DocumentTracking/cliente/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import uniquid from 'uniquid'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
+function Login(){
+
+
+    return(
+        <div >
+hjagdsgg
+            {/* <div id="login"/>
+                <h3 classname="text-center text-white pt-5">Login form</h3>
+                <div classname="container"/>
+                    <div id="login-row" classname="row justify-content-center align-items-center">
+                        <div id="login-column" classname="col-md-6">
+                            <div id="login-box" classname="col-md-12">
+                                <form id="login-form" classname="form" action="" method="post"/>
+                                    <h3 classname="text-center text-info">Login</h3>
+                                    <div classname="form-group">
+                                        <label for="username" classname="text-info">Username:</label><br/>
+                                        <input type="text" name="username" id="username" classname="form-control"/>
+                                    </div>
+                                    <div classname="form-group">
+                                        <label for="password" classname="text-info">Password:</label><br/>
+                                        <input type="text" name="password" id="password" classname="form-control"/>
+                                    </div>
+                                    <div classname="form-group">
+                                        <label for="remember-me" classname="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br/>
+                                        <input type="submit" name="submit" classname="btn btn-info btn-md" value="submit"/>
+                                    </div>
+                                    <div id="register-link" classname="text-right">
+                                        <a href="#" classname="text-info">Register here</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       */}
+        </div>
+    )
+}
+
+export default Login
