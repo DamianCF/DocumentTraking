c8d0b3e003f712abccbb3dc774a13c523180587c
Ronald Blanco <ronald.blanco.navarro@est.una.ac.cr>
2022-06-25 18:27:21 -0600

    borrar comentado a medio palo

--- DocumentTracking/cliente/src/components/tramite/TramiteIndividual.js
+++ DocumentTracking/cliente/src/components/tramite/TramiteIndividual.js
@@ -1,70 +1,100 @@
 import React, { useEffect } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 
-function TramiteIndividual({ tramite , mostrarQuitar, idDepartamento}) {
+function TramiteIndividual({ tramite, mostrarQuitar, idDepartamento }) {
 
     const navegar = useNavigate()
 
-// Para animacion de scroll al bajar
-    useEffect(()=>{
+    // Para animacion de scroll al bajar
+    useEffect(() => {
         AOS.init()
-    },[])
+    }, [])
 
     //Funcion para borrar tramite
-    function borrartramite(idTramite){
+    function borrartramite(idTramite) {
 
-            axios.post('/api/usuario/borrartramite', {idTramite: idTramite}).then(res =>{
-                //console.log(res.data)
-                alert(res.data)
-                navegar(0)
-            }).catch(err =>{
-                console.log(err)
-            })
+        axios.post('/api/usuario/borrartramite', { idTramite: idTramite }).then(res => {
+            //console.log(res.data)
+           // alert(res.data)
+            navegar(0)
+        }).catch(err => {
+            console.log(err)
+        })
+
+        // axios.get('api/usuario/obtenertramites').then(res => {
+        //     //console.log(res.data)
+        //     var tramites = res.data;
+        //     tramites.map(element => {
+        //         var tramites1 = element;
+        //         // eliminar el tramite del departamento
+        //         const filteredLibraries = tramites1.filter((item) => item !== idTramite)
+        //         //console.log(filteredLibraries)
+        //         // Nuevo objeto para actualizar usuario
+        //         const actualizardepartamento = {
+        //             idDepartamento: idDepartamento,
+        //             estado: 'A',
+        //             tramites: filteredLibraries
+        //         }
+        //         // hacer peticion usando axios
+        //         axios.post('/api/usuario/actualizardepartamento', actualizardepartamento)
+        //             .then(res => {
+        //                 console.log(res.data)
+        //                 // actualizar tramites en pantalla
+        //                 navegar(0)
+
+        //             })
+        //             .then(err => {
+        //                 if (err) {
+        //                     console.log(err)
+        //                 }
+        //             })
+        //     })
+        // }).catch(err => { console.log(err) }
+        // ) // min 1:39:38
     }
 
 
-    function quitartramite(idTramite){
+    function quitartramite(idTramite) {
         console.log(idDepartamento)
         console.log(idTramite)
 
         /// buscar dep -> cargarlo junto con sus tramites ->
-        axios.post('/api/usuario/obtenerdatadepartamento', { idDepartamento}).then(res => {
+        axios.post('/api/usuario/obtenerdatadepartamento', { idDepartamento }).then(res => {
             //console.log(res.data[0])
             const datadepartamento = res.data[0]
 
             var tramites1 = datadepartamento.tramites;
-                // eliminar el tramite del departamento
+            // eliminar el tramite del departamento
             const filteredLibraries = tramites1.filter((item) => item !== idTramite)
             //console.log(filteredLibraries)
 
 
-                    // Nuevo objeto para actualizar usuario
-        const actualizardepartamento = {
-            idDepartamento: idDepartamento,
-            estado: 'A',
-            tramites: filteredLibraries
-        }
+            // Nuevo objeto para actualizar usuario
+            const actualizardepartamento = {
+                idDepartamento: idDepartamento,
+                estado: 'A',
+                tramites: filteredLibraries
+            }
 
-        // hacer peticion usando axios
-        axios.post('/api/usuario/actualizardepartamento', actualizardepartamento)
-            .then(res => {
-                console.log(res.data)
-                // actualizar tramites en pantalla
-                navegar(0)
+            // hacer peticion usando axios
+            axios.post('/api/usuario/actualizardepartamento', actualizardepartamento)
+                .then(res => {
+                    console.log(res.data)
+                    // actualizar tramites en pantalla
+                    navegar(0)
 
-            })
-            .then(err => {
-                if (err) {
-                    console.log(err)
-                }
-            })
+                })
+                .then(err => {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
         })
 
-        // actualizar departamento con sus tramites
     }
 
     return (
@@ -72,18 +102,18 @@ function TramiteIndividual({ tramite , mostrarQuitar, idDepartamento}) {
             <div className='container'>
                 <div className='row'>
 
-                    <div className='col-sm-6 offset-3'  data-aos= "flip-right">
+                    <div className='col-sm-6 offset-3' data-aos="flip-right">
 
                         <ul className='list-group'>
-                            <li className='list-group-item'  hidden = {true}>{tramite.idTramite}</li>
+                            <li className='list-group-item' hidden={true}>{tramite.idTramite}</li>
                             <li className='list-group-item'>{tramite.descripcion}</li>
                         </ul>
 
                         <Link to={`/editartramite/${tramite.idTramite}`}><li className='btn btn-success'>Seleccionar</li></Link>
                         &nbsp;
-                        <button hidden = {mostrarQuitar} className="btn btn-warning" onClick={()=>quitartramite(tramite.idTramite)}>Quitar</button>
+                        <button hidden={mostrarQuitar} className="btn btn-warning" onClick={() => quitartramite(tramite.idTramite)}>Quitar</button>
                         &nbsp;
-                        <button className="btn btn-danger" onClick={()=>borrartramite(tramite.idTramite)}>Eliminar</button>
+                        <button className="btn btn-danger" onClick={() => borrartramite(tramite.idTramite)}>Eliminar</button>
                         <hr className='mt-4'></hr>
 
                     </div>
