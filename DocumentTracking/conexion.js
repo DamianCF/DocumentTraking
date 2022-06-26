c29a6a48419e131e5c03a00ad60682e7eedb312c
Damian Cordero Fallas <damianfallas16@gmail.com>
2022-06-25 19:10:27 -0600

    Ahora si conexion con atlas

--- DocumentTracking/conexion.js
+++ DocumentTracking/conexion.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //mongoose.connect('mongodb://127.0.0.1:27017/DocumentTraking');
-mongoose.connect('mongodb+srv://sys:sys@documenttraking.b82jfip.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://sys:sys@proyectop4.krrl4ld.mongodb.net/DocumentTracking?retryWrites=true&w=majority')
 
 const objetobd = mongoose.connection
 
